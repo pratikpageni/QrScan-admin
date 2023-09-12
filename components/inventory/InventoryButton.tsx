dfb3fecd7b679fd1c960c4f3e5226fe5e0756525
Pratik Pageni <pagenipratik@gmail.com>
2023-09-12 10:57:20 +0545

    Refactor:Inventory Button

--- components/inventory/InventoryButton.tsx
+++ components/inventory/InventoryButton.tsx
@@ -14,11 +14,11 @@ const InventoryButton = ({
   data: { icon: React.ReactNode; name: string; link: string };
 }) => {
   return (
-    <Link href={`${data.link}`} className='flex items-center'>
-      <div className='h-20 w-20 z-10 px-6 rounded-full bg-white flex items-center justify-center text-brandcolor'>
+    <Link href={`${data.link}`} className='flex items-center '>
+      <div className='h-20 w-20 z-10 px-6 rounded-full bg-white flex items-center shadow-md justify-center text-brandcolor'>
         {data.icon}
       </div>
-      <div className='px-14 py-5 rounded-[2.5rem] bg-brandcolor font-bold w-full text-white -ml-10 hover:opacity-50'>
+      <div className='px-14 py-5 rounded-[2.5rem] bg-brandcolor font-bold w-full shadow-md text-white -ml-10 hover:opacity-50'>
         {data.name}
       </div>
     </Link>
--- constraints/inventorybutton.data.tsx
+++ constraints/inventorybutton.data.tsx
@@ -1,4 +1,6 @@
-import { AiFillFolderAdd } from 'react-icons/ai';
+import { AiOutlineStock } from 'react-icons/ai';
+import { BiSolidPurchaseTagAlt } from 'react-icons/bi';
+import { GiPayMoney } from 'react-icons/gi';
 
 interface InventoryButtonInterface {
   name: string;
@@ -9,17 +11,17 @@ interface InventoryButtonInterface {
 const InventoryButtons: InventoryButtonInterface[] = [
   {
     name: 'Page1',
-    icon: <AiFillFolderAdd size={30} />,
+    icon: <BiSolidPurchaseTagAlt size={30} />,
     link: '/',
   },
   {
     name: 'Page2',
-    icon: <AiFillFolderAdd size={30} />,
+    icon: <AiOutlineStock size={30} />,
     link: '/',
   },
   {
     name: 'Page3',
-    icon: <AiFillFolderAdd size={30} />,
+    icon: <GiPayMoney size={30} />,
     link: '/',
   },
 ];
